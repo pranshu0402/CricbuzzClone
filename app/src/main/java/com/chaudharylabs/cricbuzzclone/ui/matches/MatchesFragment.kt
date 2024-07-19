8a56889536cea644d758d8d2c10a267c30e6d502
pranshu0402 <pranshu0402@gmail.com>
2024-07-19 15:57:54 +0530

    Matches tab basic setup create.

--- app/src/main/java/com/chaudharylabs/cricbuzzclone/ui/matches/MatchesFragment.kt
+++ app/src/main/java/com/chaudharylabs/cricbuzzclone/ui/matches/MatchesFragment.kt
@@ -4,19 +4,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.chaudharylabs.cricbuzzclone.R
+import com.chaudharylabs.cricbuzzclone.databinding.FragmentMatchesBinding
+import com.chaudharylabs.cricbuzzclone.ui.BaseFragment
+import com.chaudharylabs.cricbuzzclone.ui.home.matche_details.MatchesViewModel
+import com.chaudharylabs.cricbuzzclone.ui.home.matche_details.adapter.TabPagerAdapter
+import com.chaudharylabs.cricbuzzclone.ui.matches.adapter.MatchesTabPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
-class MatchesFragment : Fragment() {
+class MatchesFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentMatchesBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_matches, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_matches, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            presenter = this@MatchesFragment
+            lifecycleOwner = this@MatchesFragment
+            executePendingBindings()
+
+            setBottomNavVisibility(View.GONE)
+
+            viewPager.adapter = MatchesTabPagerAdapter(this@MatchesFragment)
+
+            val tabs = listOf(
+                this@MatchesFragment.getString(R.string.live_matches_fragment),
+                this@MatchesFragment.getString(R.string.upcoming_matches_fragment),
+                this@MatchesFragment.getString(R.string.recent_matches_fragment)
+            )
+
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = tabs[position]
+            }.attach()
+        }
+    }
+
+
     companion object {
-      private const val TAG="MatchesFragment"
+        private const val TAG = "MatchesFragment"
     }
 }
